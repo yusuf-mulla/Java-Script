3915ef0ebabc14fd462eb0aad1a13a556b2b547d
yusuf-mulla <124673307+yusuf-mulla@users.noreply.github.com>
2023-02-12 02:38:08 +0530

    chnages

--- class.js
+++ class.js
@@ -1,12 +1,25 @@
-// // var findMax=(10,20,50,600,30,8)
-// var findMax=(...arg)=>arg.reduce((item,index)=> item>index ? item :index)
-// // findMax(10,20,50,600,30,8);
-// console.log(10,20,50,600,30,8);
+function banking(amount) {
+    let notes = [2000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
+    let curruncy = Array(10).fill(0);
+    for (let i = 0; i < 10; i++) {
+      if (amount >= notes[i]) {
+        curruncy[i] = Math.floor(amount / notes[i]);
+        amount = amount % notes[i];
+      }
+    }
+    console.log("CurrencyCount ->");
+    arr.forEach(() => {
+      (notes[i] +"*"+ curruncy[i] +"="+ notes[1]*curruncy[i])
+      console.log(notes)
+    })
 
-function nm() {
-    text(3)=4
-    text1(4)=3
-    newtxt=text+text1
-    
-}
-console.log(newtxt);
+
+    // for (let i = 0; i < 10; i++) {
+    //   if (curruncy[i] != 0) {
+    //     console.log(notes[i] + "*" + curruncy[i] + "=" + notes[i] * curruncy[i]);
+    //   }
+    // }
+  }
+  let amount = 1000;
+  banking(amount);
+  
