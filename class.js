3827f30e27bd40bdedc4496e82c45d3d2c7f4967
yusuf-mulla <124673307+yusuf-mulla@users.noreply.github.com>
2023-02-11 22:43:15 +0530

    changes

---
+++ class.js
@@ -0,0 +1,12 @@
+// // var findMax=(10,20,50,600,30,8)
+// var findMax=(...arg)=>arg.reduce((item,index)=> item>index ? item :index)
+// // findMax(10,20,50,600,30,8);
+// console.log(10,20,50,600,30,8);
+
+function nm() {
+    text(3)=4
+    text1(4)=3
+    newtxt=text+text1
+    
+}
+console.log(newtxt);
